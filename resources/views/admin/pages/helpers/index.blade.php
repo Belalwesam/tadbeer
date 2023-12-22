2b1bd13f3bfe07339f3069b1bc8ca59fd7b3360b
unknown <belalwesam3@gmail.com>
2023-12-22 17:59:19 +0300

    some formatting

--- resources/views/admin/pages/helpers/index.blade.php
+++ resources/views/admin/pages/helpers/index.blade.php
@@ -74,27 +74,6 @@
 
     <div class="custome-pagination-container mt-5">
         <ul class="pagination justify-content-start" id="pagination-list-element">
-            {{--  <li class="page-item prev">
-                <a class="page-link" href="javascript:void(0);"><i class="tf-icon bx bx-chevrons-left"></i></a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" href="javascript:void(0);">1</a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" href="javascript:void(0);">2</a>
-            </li>
-            <li class="page-item active">
-                <a class="page-link" href="javascript:void(0);">3</a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" href="javascript:void(0);">4</a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" href="javascript:void(0);">5</a>
-            </li>
-            <li class="page-item next">
-                <a class="page-link" href="javascript:void(0);"><i class="tf-icon bx bx-chevrons-right"></i></a>
-            </li> --}}
         </ul>
     </div>
 
