b4d7cff983f730d70f2b16ffa5fb8f589ecd1f20
unknown <belalwesam3@gmail.com>
2023-12-23 23:22:59 +0300

    added the favicon

--- resources/views/landing/layout/app.blade.php
+++ resources/views/landing/layout/app.blade.php
@@ -9,12 +9,12 @@
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Frest | @yield('title')</title>
+    <title>Tadbeer | @yield('title')</title>
 
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('/dashboard/assets/img/favicon/favicon.ico') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('/landing/assets/images/tadbeer-logo.png') }}" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
--- resources/views/landing/pages/index.blade.php
+++ resources/views/landing/pages/index.blade.php
@@ -1,6 +1,6 @@
 @extends('landing.layout.app')
 @section('title')
-    Cleaning Services
+    
 @endsection
 @section('content')
     <button type="button" class="btn btn-success">Success</button>
