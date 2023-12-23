f9b0b955c00ae23106a558ce8df01be204094bdc
unknown <belalwesam3@gmail.com>
2023-12-23 23:20:47 +0300

    added the title

--- resources/views/landing/pages/index.blade.php
+++ resources/views/landing/pages/index.blade.php
@@ -1,4 +1,7 @@
 @extends('landing.layout.app')
+@section('title')
+    Cleaning Services
+@endsection
 @section('content')
     <button type="button" class="btn btn-success">Success</button>
 @endsection
